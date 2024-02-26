3a935b0b0c2f2e2b46f150f3904d7a888a617788
RadMa <13coolguy37@gmail.com>
2024-02-26 18:47:57 +0500

    Added the wheel

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Toy t1 = new Toy(1, "toy1", 1, 1);
-        Toy t2 = new Toy(1, "toy2", 1, 1);
-        Toy t3 = new Toy(1, "toy3", 1, 1);
+        Toy t1 = new Toy(1, "toy1", 2, 20);
+        Toy t2 = new Toy(2, "toy2", 2, 20);
+        Toy t3 = new Toy(3, "toy3", 6, 60);
 
         WheelOfFortune wheel = new WheelOfFortune();
 
         wheel.Add(t1);
         wheel.Add(t2);
         wheel.Add(t3);
-        wheel.Test();
+        wheel.SpinTheWheel(10);
     }
 }
--- WheelOfFortune.java
+++ WheelOfFortune.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
-import java.util.random.*;
+import java.util.Random;
 
 public class WheelOfFortune{
 
     private ArrayList<Toy> wheel;
     private int chanceCount;
+    private ArrayList<Integer> table = new ArrayList<>();
+    private ArrayList<Toy> winners = new ArrayList<>();
 
     public WheelOfFortune(){
         wheel = new ArrayList<>();
@@ -12,11 +14,31 @@ public class WheelOfFortune{
     
     public void Add(Toy toy){
         wheel.add(toy);
+        chanceCount += toy.getChance();
     }
 
-    public void Test(){
-        for(Toy toy : wheel){
-            System.out.println(toy);
+
+    public void SpinTheWheel(int amountOfSpins){
+        if(chanceCount != 100){
+            System.out.println("The sum of all chances is not equal to 100.");
+        } else{
+            for(Toy toy: wheel){
+                for(int i = 0; i < toy.getChance(); i++){
+                    table.add(toy.getId());
+                }
+            }
+
+            for(int i = 0; i < amountOfSpins; i++){
+                Random rng = new Random();
+                int win = rng.nextInt(100);
+                int idWinner = table.get(win);
+                for(Toy toy: wheel){
+                    if(idWinner == toy.getId()){
+                        winners.add(toy);
+                        System.out.println(toy);
+                    }
+                }
+            }
         }
     }
 }
